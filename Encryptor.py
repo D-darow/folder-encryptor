15fd8d6f9ca838ea5e70a718c21435e00d3a866c
D-darow <dimader@bk.ru>
2024-03-16 21:14:04 +0700

    Added test and trivial implementation for generate_aes_key() method.

--- Encryptor.py
+++ Encryptor.py
@@ -4,3 +4,8 @@ class Encryptor:
     """Класс шифратора"""
     def __init__(self):
         pass
+
+    @staticmethod
+    def generate_aes_key():
+        # todo: finish generate_aes_key method later
+        return '66qDdraEmkBpT7EwkUXfTwp63erzHNKvbwaghVseXVk='
--- TestEncryptor.py
+++ TestEncryptor.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from Encryptor import Encryptor
 
 
@@ -6,6 +7,10 @@ class TestEncryptor(unittest.TestCase):
     def testEncryptorClassCreation(self):
         encryptor = Encryptor()
         self.assertIsNotNone(encryptor)
+
+    def testGenerateAesKey(self):
+        key = Encryptor.generate_aes_key()
+        self.assertEqual(44, len(key))
 
 
 if __name__ == '__main__':
