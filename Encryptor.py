74a0a960691fd1fde3b37af4d855096d3eb0c394
D-darow <dimader@bk.ru>
2024-03-17 19:23:21 +0700

    Finished encrypt_aes(). All tests passed.

--- Encryptor.py
+++ Encryptor.py
@@ -1,5 +1,6 @@
 import os
-
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
@@ -23,6 +24,20 @@ class Encryptor:
 
     @staticmethod
     def encrypt_aes(file_path, key):
-        # todo: finish encrypt_aes(file_path, key) later
+        iv = os.urandom(16)
+
+        with open(file_path, 'rb') as f:
+            plaintext = f.read()
+
+        backend = default_backend()
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+        encryptor = cipher.encryptor()
+
+        padder = padding.PKCS7(128).padder()
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
+
+        ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
+
+        # Сохранение IV вместе с зашифрованными данными
         with open(file_path, 'wb') as f:
-            f.write(os.urandom(16))
+            f.write(iv + ciphertext)
